5ff3bd989137accbab4f7b76fabc0d8784d6e41b
garethlin <garethlinsmith@gmail.com>
2021-06-26 15:32:49 +0100

    further cleanup of init. more processing in base class

--- build/lib/asm_hifi/__init__.py
+++ build/lib/asm_hifi/__init__.py
@@ -36,10 +36,20 @@ class AssemHiFi:
 		self.create_run_config()
 		self.exe_env = ExecutionEnvironment(self.args, NOW, job_suffix="assem", log_dir=os.path.join(self.hpc_logdir))
 	def read_default_config(self):
-		pass
+		assertFilenameValid(os.path.join(os.path.dirname(__file__), "etc", "asm_hifi.config.yaml"), "MISSING CONFIG ERROR: Failed to find default config")
 	def create_run_config(self):
-		pass
+		print("IN CREATE RUN CONFIG SUPERCLASS")
+		with open(self.default_config_path,'r') as yamlfile:
+			with open(self.run_config_path,'w') as run_config:
+				for line in yamlfile:
+					run_config.write(line)
+				run_config.write("output_base_dir: " + self.args.outdir + "\n")
+				print(self.args.outdir)
+				run_config.write("sample_sheet: " + self.args.sample_sheet + "\n")
+				print(self.args.sample_sheet)
 	def run(self):
+		if os.path.exists(self.done_marker_path) and self.args.rerun:
+			os.remove(self.done_marker_path)
 		if not os.path.exists(self.done_marker_path):
 			snakemake_success = run_snakemake(self.snakefile, self.snakemake_outdir, self.run_config_path, self.exe_env, dryrun=self.args.dryrun)
 			if snakemake_success:
@@ -54,26 +64,32 @@ class AssemHiFi:
 class QCModule(AssemHiFi):
 	def __init__(self,args):
 		AssemHiFi.__init__(self,args)
-	def read_default_config(self):
-		assertFilenameValid(os.path.join(os.path.dirname(__file__), "etc", "asm_hifi.config.yaml"), "MISSING CONFIG ERROR: Failed to find default config")
-	def create_run_config(self):
-		with open(self.default_config_path,'r') as yamlfile:
-			with open(self.run_config_path,'w') as run_config:
-				for line in yamlfile:
-					run_config.write(line)
-				run_config.write("output_base_dir: " + self.args.outdir + "\n")
-				run_config.write("sample_sheet: " + self.args.sample_sheet + "\n")
+	#def read_default_config(self):
+	#	assertFilenameValid(os.path.join(os.path.dirname(__file__), "etc", "asm_hifi.config.yaml"), "MISSING CONFIG ERROR: Failed to find default config")
+	#def create_run_config(self):
+	#	with open(self.default_config_path,'r') as yamlfile:
+	#		with open(self.run_config_path,'w') as run_config:
+	#			for line in yamlfile:
+	#				run_config.write(line)
+	#			run_config.write("output_base_dir: " + self.args.outdir + "\n")
+	#			run_config.write("sample_sheet: " + self.args.sample_sheet + "\n")
 
 class AssemblyModule(AssemHiFi):
 	def __init__(self,args):
 		AssemHiFi.__init__(self,args)
-	def read_default_config(self):
-		assertFilenameValid(os.path.join(os.path.dirname(__file__), "etc", "asm_hifi.config.yaml"), "MISSING CONFIG ERROR: Failed to find default config")
+	#def read_default_config(self):
+	#	assertFilenameValid(os.path.join(os.path.dirname(__file__), "etc", "asm_hifi.config.yaml"), "MISSING CONFIG ERROR: Failed to find default config")
 	def create_run_config(self):
-		with open(self.default_config_path,'r') as yamlfile:
-			with open(self.run_config_path,'w') as run_config:
-				for line in yamlfile:
-					run_config.write(line)
-				run_config.write("output_base_dir: " + self.args.outdir + "\n")
-				run_config.write("sample_sheet: " + self.args.sample_sheet + "\n")
+		AssemHiFi.create_run_config(self)
+		with open(self.run_config_path,'a') as run_config:
+			if self.args.keepbin:
+				run_config.write("HIFIASM_KEEP_BIN_FILES: True\n")
+			else:
 				run_config.write("HIFIASM_KEEP_BIN_FILES: False\n")
+		#with open(self.default_config_path,'r') as yamlfile:
+		#	with open(self.run_config_path,'w') as run_config:
+		#		for line in yamlfile:
+		#			run_config.write(line)
+		#		run_config.write("output_base_dir: " + self.args.outdir + "\n")
+		#		run_config.write("sample_sheet: " + self.args.sample_sheet + "\n")
+		#		run_config.write("HIFIASM_KEEP_BIN_FILES: False\n")
