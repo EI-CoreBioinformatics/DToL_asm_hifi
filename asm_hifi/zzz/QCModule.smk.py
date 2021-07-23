c2295b0a627280676153bfe4110fa66200e1e724
garethlin <garethlinsmith@gmail.com>
2021-07-23 13:18:57 +0100

    fix issues 3,4,11,12

--- asm_hifi/zzz/QCModule.smk.py
+++ asm_hifi/zzz/QCModule.smk.py
@@ -23,6 +23,12 @@ def get_assem_bname(wc):
 def get_alignments(wc):
 	return alignments[wc.sample][wc.assembly_basename]
 
+def get_busco_db(wc):
+	return config["busco_dbs"][kingdoms[wc.sample]]
+
+def get_results_files(wc):
+	return results_files[wc.sample]
+
 def extract_stats(statsfilename):
 	with open(statsfilename,"r") as statsfile:
 		for line in statsfile:
@@ -41,7 +47,28 @@ def extract_stats(statsfilename):
 				elif "% main genome in scaffolds > 50 KB" in fields[0]:
 					perc_over50kb = fields[1]
 
-	return num_contigs,assem_size,L50,L90,num_over50kb,perc_over50kb
+	return ",".join([num_contigs,assem_size,L50,L90,num_over50kb,perc_over50kb])
+
+def extract_qv(yakfilename):
+	with open(yakfilename, "r") as yak:
+		for line in yak:
+			if line.startswith("QV"):
+				fields = line.rstrip().split("\t")
+				qv = fields[0]
+			elif "YAK FAILED" in line:
+				qv = "YAK FAILED"
+	return qv
+
+def extract_busco(buscofilename):
+	i = 0
+	with open(buscofilename, "r") as buscofile:
+		for line in buscofile:
+			i = i + 1
+			if i == 8:
+				busco = line.rstrip().replace("\t","").replace(","," ")
+	return busco
+			
+				
 
 HPC_CONFIG = HpcConfig(DEFAULT_HPC_CONFIG_FILE)
 TARGETS = []
@@ -49,19 +76,23 @@ assemblies = {}
 reads = {}
 reads_lookup = {}
 assembly_lookup = {}
-adapter_logs = []
+#adapter_logs = []
+kingdoms = {}
+assembly_types = {}
+
 infile = open(config['sample_sheet'])
 for line in infile:
 	if line.startswith("#"):
 		continue
 	fields = line.rstrip().split(",")
 	num_fields = len(fields)
-	if num_fields != 3:
-		sys.exit("ERROR in sample sheet" + config['sample_sheet'] + ": sample sheet should contain exactly 3 comma delimted fields: sample_name,reads_fofn,assemblies_fofn")
+	if num_fields != 4:
+		sys.exit("ERROR in sample sheet" + config['sample_sheet'] + ": sample sheet should contain exactly 4 comma delimted fields: sample_name,busco_clade,reads_fofn,assemblies_fofn")
 	
 	sample_name = fields[0]
-	reads_fof_name = fields[1]
-	assem_fof_name = fields[2]
+	kingdom = fields[1]
+	reads_fof_name = fields[2]
+	assem_fof_name = fields[3]
 
 	if not os.path.isfile(reads_fof_name):
 		sys.exit("ERROR in sample sheet" + config['sample_sheet'] + ": reads fofn " + reads_fof_name + "does not exist")
@@ -72,15 +103,22 @@ for line in infile:
 	if sample_name in reads.keys():
 		sys.exit("ERROR in sample sheet" + config['sample_sheet'] + ": sample names must be unique")
 
+	if sample_name in kingdoms.keys():
+		sys.exit("ERROR in sample sheet" + config['sample_sheet'] + ": sample names must be unique")
+	else:
+		kingdoms[sample_name] = kingdom
+
 	reads[sample_name] = []
 	reads_lookup[sample_name] = {}
 	assembly_lookup[sample_name] = {}
 	assemblies[sample_name] = []
+	if not sample_name in assembly_types.keys():
+		assembly_types[sample_name] = {}
 
 	reads_fofn = open(reads_fof_name)
 	for l in reads_fofn:
 		reads_file = l.rstrip()
-		adapter_logs.append(reads_file + ".log")
+		#adapter_logs.append(reads_file + ".log")
 		if not os.path.isfile(reads_file):	
 			sys.exit("ERROR in reads fofn" + reads_fof_name + " read file " + reads_file + " does not exist")
 		reads[sample_name].append(reads_file)
@@ -90,8 +128,14 @@ for line in infile:
 		reads_lookup[sample_name][rbname] = reads_file
 	
 	assem_fofn = open(assem_fof_name)
-	for assem_file in assem_fofn:
-		assem_file = assem_file.rstrip()
+	for line in assem_fofn:
+		fields = line.rstrip().split(",")
+		if len(fields) != 2:
+			sys.exit("ERROR in assembly fofn" + assem_fof_name + " each entry must be comma delimited of the form assembly_type,assembly_file_path")
+		assem_type = fields[0]
+		assem_file = fields[1]
+		if assem_type != "all" and assem_type != "primary":
+			sys.exit("ERROR in assembly fofn" + assem_fof_name + " assembly type must be 'primary' or 'all'")
 		if not os.path.isfile(assem_file):
 			sys.exit("ERROR in assembly fofn" + assem_fof_name + " assembly file " + assem_file + " does not exist")
 		assemblies[sample_name].append(assem_file.rstrip())
@@ -99,52 +143,51 @@ for line in infile:
 		if abname in assembly_lookup[sample_name].keys():
 			sys.exit("ERROR in assembly fofn" + assem_fof_name + " assembly file basenames must be unique")
 		assembly_lookup[sample_name][abname] = assem_file
+		assembly_types[sample_name][abname] = assem_type
 
 for sample_name in reads.keys():
 	TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".ccs.yak"))
 
 alignments = {}
 summaries = []
+results_files = {}
 for sample_name in assemblies.keys():
 	haploid = 0
 	alignments[sample_name] = {}
+	results_files[sample_name] = []
 	for assembly in assemblies[sample_name]:
 		assem_bname = os.path.basename(assembly)
 		alignments[sample_name][assem_bname] = []
-		if "L0" in assem_bname:
-			haploid = 1
-		if "all.contigs" in assem_bname:
+		if assembly_types[sample_name][assem_bname] == "all":
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"yak.qv.out"))
+			results_files[sample_name].append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"yak.qv.out"))
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"asset.gaps.bed"))
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"asset.pb.bed"))
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"asset.ev.pb.bed"))
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"asset.pchlst.ctg.bed"))
+			results_files[sample_name].append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"asset.pchlst.ctg.bed"))
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"asset.primary.pchlst.ctg.bed"))
 		TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"kat22.comp.stats"))
-		if "p_ctg" in assem_bname:
+		if assembly_types[sample_name][assem_bname] == "all":
 			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"stats.txt"))
+			results_files[sample_name].append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,"stats.txt"))
+		TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,assem_bname + ".full_table"))
+		TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,assem_bname + ".duplication_summary"))
+		results_files[sample_name].append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,assem_bname + ".duplication_summary"))
 		for reads_file in reads[sample_name]:
 			read_bname = os.path.basename(reads_file)
-			if "all.contigs" in assem_bname: 
+			TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,read_bname + ".bam"))
+			if assembly_types[sample_name][assem_bname] == "all": 
 				TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,read_bname + ".paf"))
 				alignments[sample_name][assem_bname].append(os.path.join(config['output_base_dir'],sample_name,"QC",assem_bname,read_bname + ".paf"))
 
-	TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".collate.adapter.logs"))
-	TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".adapter.summary.txt"))
-
-	if haploid == 1:
-		TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".haploid.qc.summary.txt"))
-		summaries.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".haploid.qc.summary.txt"))
-	else:
-		TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".qc.summary.txt"))
-		summaries.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".qc.summary.txt"))
-		TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".qc.report.html"))
+	TARGETS.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".qc.summary.txt"))
+	summaries.append(os.path.join(config['output_base_dir'],sample_name,"QC",sample_name + ".qc.summary.txt"))
 
 TARGETS.append(os.path.join(config['output_base_dir'],"Results","Summary.txt"))
 
-print(adapter_logs)
 		
-localrules: all, asset_step1, asset_step2, asset_step3, asset_step4, master_summary, parse_adapter_summary, sum_adapter, summary, summary2html
+localrules: all, asset_step1, asset_step2, asset_step3, asset_step4, master_summary, summary
 
 rule all:
 	input: TARGETS
@@ -157,7 +200,7 @@ rule yak_hash:
 	params:
 		load = loadPreCmd(config["load"]["yak"])
 	threads:
-		16
+		32
 	resources:
 		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("yak_hash") * attempt
 	shell:
@@ -173,7 +216,7 @@ rule yak_qv:
 	params:
 		load = loadPreCmd(config["load"]["yak"])
 	threads:
-		16
+		32
 	resources:
 		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("yak_qv") * attempt
 	shell:
@@ -191,6 +234,21 @@ rule minimap2paf:
 		16
 	resources:
 		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("minimap2paf") * attempt
+	shell:
+		"{params.load} minimap2 -t {threads} -x asm20 {input[1]} {input[0]} > {output} || echo 'MINIMAP FAILED' > {output}"
+
+rule minimap2bam:
+	input:
+		get_read_file,
+		get_assembly
+	output:
+		os.path.join(config['output_base_dir'],"{sample}","QC","{assembly_basename}","{read_basename}" + ".bam")
+	params:
+		load = loadPreCmd(config["load"]["minimap2"],config["load"]["samtools"])
+	threads:
+		16
+	resources:
+		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("minimap2bam") * attempt
 	shell:
 		"{params.load} minimap2 -t {threads} -x asm20 {input[1]} {input[0]} > {output} || echo 'MINIMAP FAILED' > {output}"
 
@@ -285,44 +343,12 @@ rule kat:
 		sample = get_sample,
 		assem_bname = get_assem_bname
 	threads:
-		16
+		32
 	resources:
 		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("kat") * attempt
 	shell:
 		"{params.load} kat comp -t {threads} -m 22 -o {params.outdir}/{params.sample}/QC/{params.assem_bname}/kat22.comp '{input.reads}' {input.assembly} || echo 'KAT FAILED' > {output}"
 
-rule parse_adapter_summary:
-	input:
-		adapter_logs
-	output:
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.collate.adapter.logs")
-	threads:
-		1
-	shell:
-		"cat {input} | grep \"Total reads processed\|Reads with adapters\" | sed 's/ //g' | sed 's/,//g' | cut -f 1 -d '(' | sed 's/:/\t/' > {output}"
-
-rule sum_adapter:
-	input:
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.collate.adapter.logs")
-	output:
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.adapter.summary.txt")
-	threads:
-		1
-	run:
-		total_reads = 0
-		total_adapters = 0
-		with open(output[0],"w") as outfile:
-			with open(input[0], "r") as infile:
-				for line in infile:
-					fields = line.rstrip().split("\t")
-					if "Totalreadsprocessed" in line:
-						num_reads = int(fields[1])
-						total_reads = total_reads + num_reads
-					if "Readswithadapters" in line:
-						num_adapters = int(fields[1])
-						total_adapters = total_adapters + num_adapters
-			outfile.write(str(total_reads) + "\t" + str(total_adapters) + "\n")
-			
 rule asm_stats:
 	input:
 		get_assembly
@@ -337,48 +363,28 @@ rule asm_stats:
 	shell:
 		"{params.load} stats.sh {input} > {output}"
 
-rule summary_haploid:
+rule busco:
 	input:
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L0.all.contigs.fasta","yak.qv.out"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L0.all.contigs.fasta","asset.primary.pchlst.ctg.bed"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L0.p_ctg.fasta","stats.txt"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.adapter.summary.txt")
+		get_assembly
 	output:
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.haploid.qc.summary.txt")
+		full_table = os.path.join(config['output_base_dir'],"{sample}","QC","{assembly_basename}","{assembly_basename}.full_table"),
+		short_sum = os.path.join(config['output_base_dir'],"{sample}","QC","{assembly_basename}","{assembly_basename}.duplication_summary")
 	params:
-		sample_name = get_sample
+		busco_db = lambda wc:get_busco_db(wc),
+		outdir = config['output_base_dir'],
+		load = loadPreCmd(config["load"]["busco"]),
+		assem_bname = get_assem_bname
 	threads:
-		1
+		8
 	resources:
-		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("summary_haploid") * attempt
-	run:
-		with open(input[0], "r") as yak:
-			for line in yak:
-				if line.startswith("QV"):
-					fields = line.rstrip().split("\t")
-					qvL0 = fields[0]
-		breaksL0 = sum(1 for _ in open(input[1]))
-		num_contigs_L0, assem_size_L0, L50_L0, L90_L0, num_over50kb_L0, perc_over50kb_L0 = extract_stats(input[2])
+		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("busco")
+	shell:
+		"{params.load} mkdir -p {params.outdir}/BUSCO_RUN/TMP && cd {params.outdir}/BUSCO_RUN && run_BUSCO.py -f -z -c {threads} -i {input} -l {params.busco_db} -o {params.assem_bname} -m genome -t {params.outdir}/BUSCO_RUN && cp {params.outdir}/BUSCO_RUN/run_{params.assem_bname}/short_summary_{params.assem_bname}.txt {output.short_sum} && cp {params.outdir}/BUSCO_RUN/run_{params.assem_bname}/full_table_{params.assem_bname}.tsv {output.full_table}"
 
-		with open(input[3], "r") as adapt:
-			for line in adapt:
-				fields = line.rstrip().split("\t")
-				num_reads = fields[0]
-				reads_adapt = fields[1]
-
-		with open(output[0],"w") as outfile:
-			outfile.write(",".join(["#Sample,NumReads,NumReadsAdapter,PurgeLevel,QV,NumContigs,AssemblySizeize,N50,N90,NumContigs>50kb,PercentAssembly>50kb,NumBreakpoints\n"]))
-			outfile.write(",".join([params.sample_name,num_reads,reads_adapt,"L0",qvL0,num_contigs_L0, assem_size_L0, L50_L0, L90_L0, num_over50kb_L0, perc_over50kb_L0,breaksL0 + "\n"]))
 
 rule summary:
 	input:
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L1.all.contigs.fasta","yak.qv.out"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L1.all.contigs.fasta","asset.primary.pchlst.ctg.bed"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L1.p_ctg.fasta","stats.txt"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L2.all.contigs.fasta","yak.qv.out"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L2.all.contigs.fasta","asset.primary.pchlst.ctg.bed"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.L2.p_ctg.fasta","stats.txt"),
-		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.adapter.summary.txt")
+		results = get_results_files
 	output:
 		os.path.join(config['output_base_dir'],"{sample}","QC","{sample}.qc.summary.txt")
 	params:
@@ -388,45 +394,52 @@ rule summary:
 	resources:
 		mem_mb = lambda wildcards, attempt: HPC_CONFIG.get_memory("summary") * attempt
 	run:
-		with open(input[0], "r") as yak:
-			for line in yak:
-				if line.startswith("QV"):
-					fields = line.rstrip().split("\t")
-					qvL1 = fields[0]
-				elif "YAK FAILED" in line:
-					qvL1 = "YAK FAILED"
-		with open(input[3], "r") as yak:
-			for line in yak:
-				if line.startswith("QV"):
-					fields = line.rstrip().split("\t")
-					qvL2 = fields[0]
-				elif "YAK FAILED" in line:
-					qvL2 = "YAK FAILED"
-
-		breaksL1 = str(sum(1 for _ in open(input[1])))
-		breaksL2 = str(sum(1 for _ in open(input[4])))
-
-		num_contigs_L1, assem_size_L1, L50_L1, L90_L1, num_over50kb_L1, perc_over50kb_L1 = extract_stats(input[2])
-		num_contigs_L2, assem_size_L2, L50_L2, L90_L2, num_over50kb_L2, perc_over50kb_L2 = extract_stats(input[5])
-		
-		with open(input[6], "r") as adapt:
-			for line in adapt:
-				fields = line.rstrip().split("\t")
-				num_reads = fields[0]
-				reads_adapt = fields[1]
+		result_data = {}
+		print("IN RUN")
+		for filename in input.results:
+			print("PICKED " + filename)
+			result_basename = os.path.basename(filename)
+			assembly_basename = os.path.basename(os.path.dirname(filename))
+			if assembly_basename not in result_data.keys():
+				result_data[assembly_basename] = {}
+				result_data[assembly_basename]['qv'] = ""
+				result_data[assembly_basename]['breaks'] = ""
+				result_data[assembly_basename]['stats'] = ",,,,,"
+			if result_basename == "yak.qv.out":
+				print("FOUND YAK")
+				result_data[assembly_basename]['qv'] = extract_qv(filename)
+				print(result_data[assembly_basename]['qv'])
+				print("DONE YAK")
+			if result_basename == "asset.pchlst.ctg.bed":
+				print("FOUND ASSET")
+				result_data[assembly_basename]['breaks'] = str(sum(1 for _ in open(filename)))
+				print(result_data[assembly_basename]['breaks'])
+				print("DONE ASSETT")
+			if result_basename == "stats.txt":
+				print("FOUND STATS")
+				result_data[assembly_basename]['stats'] = extract_stats(filename) 
+				print(result_data[assembly_basename]['stats'])
+				print("DONE STATS")
+			if result_basename.endswith("duplication_summary"):
+				print("FOUND BUSCO")
+				result_data[assembly_basename]['busco'] = extract_busco(filename)
+				print(result_data[assembly_basename]['busco'])
+				print("DONE BUSCO")
+				
 		
 		with open(output[0],"w") as outfile:
-			outfile.write(",".join(["#Sample,NumReads,NumReadsAdapter,PurgeLevel,QV,NumContigs,AssemblySizeize,N50,N90,NumContigs>50kb,PercentAssembly>50kb,NumBreakpoints\n"]))
-			outfile.write(",".join([params.sample_name,num_reads,reads_adapt,"L1",qvL1,num_contigs_L1, assem_size_L1, L50_L1, L90_L1, num_over50kb_L1, perc_over50kb_L1,breaksL1 + "\n"]))
-			outfile.write(",".join([params.sample_name,num_reads,reads_adapt,"L2",qvL2,num_contigs_L2, assem_size_L2, L50_L2, L90_L2, num_over50kb_L2, perc_over50kb_L2,breaksL2 + "\n"]))
+			outfile.write(",".join(["#Sample,Assembly,QV,NumContigs,AssemblySize,N50,N90,NumContigs>50kb,PercentAssembly>50kb,NumBreakpoints,BUSCO\n"]))
+			outfile.write(",".join([params.sample_name,assembly_basename,result_data[assembly_basename]['qv'],result_data[assembly_basename]['stats'],result_data[assembly_basename]['breaks'],result_data[assembly_basename]['busco'] + "\n"]))
 
 rule master_summary:
 	input:
 		summaries
 	output:
 		os.path.join(config['output_base_dir'],"Results","Summary.txt")
+	params:
+		outdir = config['output_base_dir'],
 	threads:
 		1
 	shell:
-		"echo '#Sample,PurgeLevel,QV,NumContigs,AssemblySizeize,N50,NumContigs>50kb,PercentAssembly>50kb,NumBreakpoints' > {output} && cat {input} | grep -v '#' >> {output}"
+		"mkdir -p {params.outdir}/Results && echo '#Sample,Assembly,QV,NumContigs,AssemblySize,N50,NumContigs>50kb,PercentAssembly>50kb,NumBreakpoints' > {output} && cat {input} | grep -v '#' >> {output}"
 			
